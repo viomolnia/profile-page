2fb05fc290e05b68e27c24420b5c57a130919ff7
Anna Hrunova <Anna.Hrunova@4finance.com>
2017-04-06 16:25:52 +0300

    improving code style

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Profile Page</title>
-    <style type="text/css">
-        <?php include 'design.css'; ?>
-    </style></head>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Profile Page</title>
+        <style type="text/css">
+            <?php include 'design.css'; ?>
+        </style>
+        <title>Profile</title>
+    </head>
 
     <body>
-        <header>
-            <a href="index.php">Profile</a>
-        </header>
-
         <div class="main">
             <div class="advertising">
                 <ul>
@@ -39,7 +37,7 @@
             </div>
 
             <div class="main-info">
-                <img src="https://s-media-cache-ak0.pinimg.com/736x/84/a8/bd/84a8bd9619648b1c67e72a7b4a5d06df.jpg">
+                <img src="https://s-media-cache-ak0.pinimg.com/736x/84/a8/bd/84a8bd9619648b1c67e72a7b4a5d06df.jpg" alt="Profile picture is not available now">
                 <table>
                     <tr>
                         <td class="field-name">Name: </td>
@@ -73,7 +71,7 @@
                 </table>
             </div>
 
-            <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&width=450&layout=standard&action=like&show_faces=true&share=true&height=80&appId" width="450" height="80" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+            <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&width=450&layout=standard&action=like&show_faces=true&share=true&height=80&appId" width="450" height="80" style="border:none;overflow:hidden" scrolling="no" frameborder="0" <!--[if IE] allowTransparency="true" --> />
         </div>
 
         <div class="footer">
